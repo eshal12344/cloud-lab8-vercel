d435d795dbddeeab0f258e502e6ec7fdbbb836ab
Eshal <eshal@gmail.com>
2026-05-15 23:19:52 +0500

    Lab 8 initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "10%" }}>
+      <h1>Lab 8 — Vercel Deployment</h1>
+      <p>Name: [ESHAL ATIF]</p>
+      <p>Roll: [01-131232-023]</p>
+    </main>
+  );
+}
